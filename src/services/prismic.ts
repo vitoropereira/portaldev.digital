26ad037b95c5ab6f0e5c8b4dae58494160292a71
Vitor <vop1234@hotmail.com>
2022-03-23 22:33:35 -0300

    credential

--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -1,10 +1,9 @@
 import Prismic from "@prismicio/client";
 
 export function getPrismicClient(req?: unknown) {
-  const prismic = Prismic.client("https://portaldev2021.prismic.io/api/v2", {
+  const prismic = Prismic.client(process.env.PRISMIC_ENDPOINT, {
     req,
-    accessToken:
-      "MC5ZamVlQmhBQUFDRUFvd0Nk.Fe-_vQ4a77-9OwHvv70E77-977-977-9TO-_ve-_vVR2PFnvv73vv71j77-977-9Ve-_vRUyee-_ve-_vSI",
+    accessToken: process.env.PRISMIC_ACCESS_TOKEN,
   });
 
   return prismic;
