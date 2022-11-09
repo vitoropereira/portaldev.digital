eb9984ccc1cbcf56dc2a3373d3aeeefa14f71a36
VITOR PEREIRA <VOP1234@HOTMAIL.COM>
2022-11-08 21:58:34 -0300

    :construction: COMMENT SOCIAL LOGIN

--- src/components/SignInGithub/index.tsx
+++ src/components/SignInGithub/index.tsx
@@ -1,37 +1,37 @@
-import { FaGithub } from "react-icons/fa";
-import { FiX } from "react-icons/fi";
-import { signIn, signOut, useSession } from "next-auth/client";
+// import { FaGithub } from "react-icons/fa";
+// import { FiX } from "react-icons/fi";
+// import { signIn, signOut, useSession } from "next-auth/client";
 
-import styles from "./styles.module.scss";
-import Router from "next/router";
+// import styles from "./styles.module.scss";
+// import Router from "next/router";
 
-export function SingInGithub() {
-  const [session] = useSession();
-  console.log("session");
-  console.log(session);
+// export function SingInGithub() {
+//   const [session] = useSession();
+//   console.log("session");
+//   console.log(session);
 
-  // if (session) {
-  //   Router.push("../");
-  // }
+//   // if (session) {
+//   //   Router.push("../");
+//   // }
 
-  return session ? (
-    <button
-      type="button"
-      className={styles.sigInButton}
-      onClick={() => signOut()}
-    >
-      <FaGithub color="#04d361" />
-      {session.user.name}
-      <FiX color="#737380" className={styles.closeIcon} />
-    </button>
-  ) : (
-    <button
-      type="button"
-      className={styles.sigInButton}
-      onClick={() => signIn("github")}
-    >
-      <FaGithub color="#eba417" />
-      Sign in with GitHub
-    </button>
-  );
-}
+//   return session ? (
+//     <button
+//       type="button"
+//       className={styles.sigInButton}
+//       onClick={() => signOut()}
+//     >
+//       <FaGithub color="#04d361" />
+//       {session.user.name}
+//       <FiX color="#737380" className={styles.closeIcon} />
+//     </button>
+//   ) : (
+//     <button
+//       type="button"
+//       className={styles.sigInButton}
+//       onClick={() => signIn("github")}
+//     >
+//       <FaGithub color="#eba417" />
+//       Sign in with GitHub
+//     </button>
+//   );
+// }
