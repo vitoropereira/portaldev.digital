f7eb4a66ebcfb49bb0a089c3ed95db1e2a6e992c
VITOR PEREIRA <VOP1234@HOTMAIL.COM>
2022-11-19 14:00:41 -0300

    :bug: Correct component Avatar in forumComment

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -9,7 +9,7 @@ interface AvatarProps extends ImageProps {
 export function Avatar({ src, hasBorder = true, ...props }: AvatarProps) {
   return (
     <span className={hasBorder ? styles.avatarWithBOrder : styles.avatar}>
-      <Image src={src} alt="Perfil Imagem" height="60" width="60" {...props} />
+      <Image src={src} height="60" width="60" alt="" {...props} />
     </span>
   );
 }
